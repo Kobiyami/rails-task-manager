dc7ff289b2e4d01d7fc8cad7bc99c973c8e9ce7d
Guillaume Fournier <fournierg01@gmail.com>
2024-11-26 14:55:24 +0100

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,32 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/tasks", to: "tasks#index"
+
+  # Nouvelle tâche (formulaire)
+  get "/tasks/new", to: "tasks#new"
+  # Nouvelle tâche (tâche créée)
+  post "/tasks", to: "tasks#create"
+
+  # Index des tasks
+  get "/tasks", to: "tasks#index"
+
+  # Edit d'une tâche (formulaire)
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # Update
+  patch "/tasks/:id", to: "tasks#update"
+
+  # Destroy une tâche
+  delete "/tasks/:id", to: "tasks#destroy"
+
+  get "/tasks/:id", to: "tasks#show", as: :task
+
 end
+
+
+
+
+  # get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # resources :restaurants # , only: [:new, :create]
